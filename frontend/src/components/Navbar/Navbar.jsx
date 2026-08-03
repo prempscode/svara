24ed0db3b4d4fba03b2c22d0f447cbecfd5e142e
premdev <prempravash65@gmail.com>
2026-08-03 10:13:00 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from 'react-router-dom'
 import {
   FiMusic,
   FiHome,
@@ -7,25 +7,25 @@ import {
   FiUpload,
   FiPlusSquare,
   FiUser,
-  FiLogOut,
-} from "react-icons/fi";
+  FiLogOut
+} from 'react-icons/fi'
 
-import { useAuth } from "../../context/AuthContext";
-import styles from "./Navbar.module.css";
+import { useAuth } from '../../context/AuthContext'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
-  const { user, logout } = useAuth();
-  const navigate = useNavigate();
+  const { user, logout } = useAuth()
+  const navigate = useNavigate()
 
-  async function handleLogout() {
-    await logout();
-    navigate("/login");
+  async function handleLogout () {
+    await logout()
+    navigate('/login')
   }
 
   return (
     <header className={styles.navbar}>
       <div className={styles.container}>
-        <NavLink to="/" className={styles.logo}>
+        <NavLink to='/' className={styles.logo}>
           <div className={styles.logoIcon}>
             <FiMusic size={18} />
           </div>
@@ -34,21 +34,21 @@ const Navbar = () => {
         </NavLink>
 
         <nav className={styles.nav}>
-          <NavLink to="/" end>
+          <NavLink to='/' end>
             Home
           </NavLink>
 
-          <NavLink to="/liked">Liked</NavLink>
+          <NavLink to='/liked'>Liked</NavLink>
 
-          <NavLink to="/albums">Albums</NavLink>
+          <NavLink to='/albums'>Albums</NavLink>
 
-          <NavLink to="/upload">Upload</NavLink>
+          <NavLink to='/upload'>Upload</NavLink>
 
-          <NavLink to="/create-album">Create Album</NavLink>
+          <NavLink to='/create-album'>Create Album</NavLink>
         </nav>
 
         <div className={styles.right}>
-          <NavLink to="/profile" className={styles.profile}>
+          <NavLink to='/profile' className={styles.profile}>
             <FiUser size={18} />
             <span>{user?.username}</span>
           </NavLink>
@@ -59,7 +59,7 @@ const Navbar = () => {
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
