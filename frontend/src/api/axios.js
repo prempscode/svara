5e5cbc2becd5939add08010a228fd2f88f6a77ec
premdev <prempravash65@gmail.com>
2026-08-03 10:10:00 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios'
 
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  withCredentials: true,
-});
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  withCredentials: true
+})
