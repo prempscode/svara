dfe1417f525ae29a849af83c08d374d0f5dfac89
premdev <prempravash65@gmail.com>
2026-08-23 22:47:22 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors'); 
-const authRoutes = require('./routes/auth.routes');
-const musicRoutes = require('./routes/music.routes');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/auth.routes");
+const musicRoutes = require("./routes/music.routes");
 
 const app = express();
 
-
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth', authRoutes);
-app.use('/api/music', musicRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/music", musicRoutes);
 
 module.exports = app;
