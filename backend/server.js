88c04bda554ec7147574c3e8367897f9e9de69d6
premdev <prempravash65@gmail.com>
2026-08-23 22:47:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await connectDB();
-
     app.listen(port, () => {
       console.log(`App listening on port ${port}!`);
     });
