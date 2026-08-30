959a4354866328adb9dd50a51fa7e972f5608cc6
premdev <prempravash65@gmail.com>
2026-08-30 10:34:00 +0530

    Update profileService.js

--- frontend/src/services/profileService.js
+++ frontend/src/services/profileService.js
@@ -1,21 +1,21 @@
-import api from "../api/axios";
+import api from '../api/axios'
 
-export async function getProfile() {
-  const response = await api.get("/auth/profile");
-  return response.data;
+export async function getProfile () {
+  const response = await api.get('/auth/profile')
+  return response.data
 }
 
-export async function updateProfile(formData) {
-  const response = await api.patch("/auth/profile", formData, {
+export async function updateProfile (formData) {
+  const response = await api.patch('/auth/profile', formData, {
     headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+      'Content-Type': 'multipart/form-data'
+    }
+  })
 
-  return response.data;
+  return response.data
 }
 
-export async function getUserProfile(userId) {
-  const response = await api.get(`/auth/profile/${userId}`);
-  return response.data;
+export async function getUserProfile (userId) {
+  const response = await api.get(`/auth/profile/${userId}`)
+  return response.data
 }
